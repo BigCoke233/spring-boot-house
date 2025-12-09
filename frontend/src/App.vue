77d625b06aa8ac80532e1721b2c631f93689d630
Eltrac <hi@guhub.cn>
2025-12-09 20:38:06 +0800

    feat(frontend): add footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterView } from 'vue-router'
 
 import NavBar from './components/NavBar.vue'
+import SiteFooter from './components/SiteFooter.vue'
 </script>
 
 <template>
@@ -9,4 +10,5 @@ import NavBar from './components/NavBar.vue'
   <main class="my-8">
     <RouterView />
   </main>
+  <SiteFooter />
 </template>
---
+++ frontend/src/components/SiteFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <footer class="w-full h-xs mt-xs absolute bottom-0 bg-gray">
+    <div>
+      test
+    </div>
+  </footer>
+</template>
