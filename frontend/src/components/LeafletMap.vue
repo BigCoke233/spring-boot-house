0a182f3686938b4e96f8d15fcac989680c3b2bce
Eltrac <hi@guhub.cn>
2025-12-20 21:32:34 +0800

    refactor(LeafletMap): 将地图中心和缩放级别改为可配置的 props

--- frontend/src/components/LeafletMap.vue
+++ frontend/src/components/LeafletMap.vue
@@ -1,10 +1,21 @@
 <script setup>
 import { VMap, VMapOsmTileLayer, VMapZoomControl } from 'vue-map-ui';
+
+const props = defineProps({
+  center: {
+    type: Array,
+    default: () => [48.8566, 2.3522]
+  },
+  zoom: {
+    type: Number,
+    default: 18
+  }
+})
 </script>
 
 <template>
   <div class="rd-lg overflow-hidden shadow">
-    <VMap :center="[48.8566, 2.3522]" :zoom="18" style="height: 500px;">
+    <VMap :center="props.center" :zoom="props.zoom" style="height: 500px;">
       <VMapOsmTileLayer />
       <VMapZoomControl />
     </VMap>
