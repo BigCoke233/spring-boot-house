93a9715f7f34838410beb58017b0881ce66d8c27
司秦 <3382572688@qq.com>
2025-12-23 19:36:09 +0800

    更改数据控制，改为xml接口控制

--- backend/src/main/java/com/zgqf/house/mapper/SellerMapper.java
+++ backend/src/main/java/com/zgqf/house/mapper/SellerMapper.java
@@ -9,15 +9,11 @@ public interface SellerMapper {
     /**
      * 根据ID查询卖家信息
      */
-    @Select("SELECT * FROM seller WHERE s_id = #{sellerId}")
     Seller selectById(@Param("sellerId") Integer sellerId);
 
     /**
      * 更新卖家信息
      */
-    @Update("UPDATE seller SET s_name = #{sName}, s_describe = #{sDescribe}, " +
-            "s_phone = #{sPhone}, s_email = #{sEmail}, s_website = #{sWebsite} " +
-            "WHERE s_id = #{sId}")
     int update(Seller seller);
 }
 
