1896cd63dcee7b8c18d9b53842ebfba0a69c037f
Eltrac <hi@guhub.cn>
2025-12-24 14:40:51 +0800

    feat: 添加EnableSpringDataWebSupport注解以支持分页序列化

--- backend/src/main/java/com/zgqf/house/HouseApplication.java
+++ backend/src/main/java/com/zgqf/house/HouseApplication.java
@@ -2,8 +2,10 @@ package com.zgqf.house;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class HouseApplication {
 
 	public static void main(String[] args) {
